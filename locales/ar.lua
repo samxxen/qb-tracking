60762bef89e306fcadaba339cfec2e296393d769
sam xxen <samxxen@gmail.com>
2025-06-01 12:04:12 +0300

    ar.lua

---
+++ locales/ar.lua
@@ -0,0 +1,21 @@
+return {
+    
+    ['tracking_started'] = 'تم بدء تعقب الهدف',
+    ['tracking_ended'] = 'انتهى وقت التعقب',
+    ['tracking_blocked'] = 'تم إحباط محاولة تعقبك',
+    ['tracking_success'] = 'تم تعقب موقعك!',
+    ['no_permission'] = 'ليست لديك صلاحية استخدام الجهاز',
+    ['player_offline'] = 'اللاعب غير متصل أو الرقم غير صحيح',
+    ['tracking_failed'] = 'فشل تعقب الهدف، لقد أحبط المحاولة',
+    
+    
+    ['tracking_title'] = 'نظام تعقب الأفراد',
+    ['phone_input_label'] = 'رقم الهاتف',
+    ['phone_input_desc'] = 'أدخل رقم الهاتف المراد تعقبه',
+    
+   
+    ['blip_name'] = 'موقع التعقب',
+    
+    
+    ['use_tracker'] = 'استخدام جهاز التعقب',
+}
