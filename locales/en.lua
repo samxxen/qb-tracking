cc8353611588b1bd0791e10cdfa1f8d527627734
sam xxen <samxxen@gmail.com>
2025-06-01 12:04:34 +0300

    en.lua

---
+++ locales/en.lua
@@ -0,0 +1,21 @@
+return {
+    
+    ['tracking_started'] = 'Started tracking target',
+    ['tracking_ended'] = 'Tracking time has ended',
+    ['tracking_blocked'] = 'You blocked the tracking attempt',
+    ['tracking_success'] = 'Your location has been tracked!',
+    ['no_permission'] = 'You are not authorized to use this device',
+    ['player_offline'] = 'Player is offline or number is invalid',
+    ['tracking_failed'] = 'Tracking failed, target blocked the attempt',
+    
+    
+    ['tracking_title'] = 'Personnel Tracking System',
+    ['phone_input_label'] = 'Phone Number',
+    ['phone_input_desc'] = 'Enter the phone number to track',
+    
+    
+    ['blip_name'] = 'Tracking Location',
+    
+    
+    ['use_tracker'] = 'Use Tracking Device',
+}
