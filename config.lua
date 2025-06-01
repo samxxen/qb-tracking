6fae47292e2263ea636d5c3714aad70033f01c56
sam xxen <samxxen@gmail.com>
2025-06-01 12:20:28 +0300

    config.lua

--- config.lua
+++ config.lua
@@ -1,13 +1,23 @@
-Config = {}
+Config = {
+    --  (ar or en)
+    Locale = 'ar',
+    
+    TrackerItem = 'tracker',
+    
+    AllowedJobs = {
+        'police',
+        'sheriff',
+        'fbi'
+    },
+    
 
-Config.Language = 'ar' -- 'ar'or'en' 
-
-Config.Tracking = {
-    Duration = 30000, 
-    Radius = 30.0,    
-    Marker = {
-        Type = 28,    
-        Size = 2.0,  
-        Color = {r = 255, g = 0, b = 0, a = 150} 
+    Tracking = {
+        Duration = 30000,
+        Radius = 30.0,
+        Marker = {
+            Type = 28,
+            Size = 2.0,
+            Color = {r = 255, g = 0, b = 0, a = 150}
+        }
     }
 }
