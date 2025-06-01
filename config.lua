82a5d535706b2729a8ace4731759d6686120c830
sam xxen <samxxen@gmail.com>
2025-06-01 12:01:45 +0300

    config.lua

---
+++ config.lua
@@ -0,0 +1,13 @@
+Config = {}
+
+Config.Language = 'ar' -- 'ar'or'en' 
+
+Config.Tracking = {
+    Duration = 30000, 
+    Radius = 30.0,    
+    Marker = {
+        Type = 28,    
+        Size = 2.0,  
+        Color = {r = 255, g = 0, b = 0, a = 150} 
+    }
+}
